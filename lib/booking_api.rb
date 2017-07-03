9216d4ebe7ef07b018c5d3b0e4a95c7e64240dee
Eugeniu Tambur <eugeniu@chatbots2go.com>
2017-07-03 18:26:09 +0200

    added rooms functionality

--- lib/booking_api.rb
+++ lib/booking_api.rb
@@ -5,6 +5,7 @@ require 'active_support/core_ext/hash/indifferent_access'
 
 require 'booking_api/version'
 require 'booking_api/hotel'
+require 'booking_api/room'
 require 'booking_api/location'
 require 'booking_api/http_service'
 require 'booking_api/images'
---
+++ lib/booking_api/room.rb
@@ -0,0 +1,33 @@
+module BookingApi
+  class Room
+
+    def initialize
+      @http_service = HttpService.new
+    end
+
+    def http_service
+      @http_service
+    end
+
+    # gets hotel photos for the given hotel ids
+    def get_room_photos(room_ids: [], request_parameters: {})
+      raise ArgumentError if room_ids.empty?
+      default_parameters = {
+        room_ids: room_ids.join(",")
+      }
+      http_service.request_get("/json/bookings.getRoomPhotos", default_parameters.merge(request_parameters))
+    end
+
+    def get_room_translations(room_ids: [], request_parameters: {})
+      raise ArgumentError if room_ids.empty?
+      default_parameters = {
+        room_ids: room_ids.join(",")
+      }
+      http_service.request_get("/json/bookings.getRoomTranslations", default_parameters.merge(request_parameters))
+    end
+
+    private
+
+  end
+end
+
