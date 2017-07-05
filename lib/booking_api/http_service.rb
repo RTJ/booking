240f401991b61fd197801bf2dd827f4993b8d44f
Eugeniu Tambur <eugeniu@chatbots2go.com>
2017-07-05 15:28:18 +0200

    added secure request, to handle payments, reservation and cancelation

--- lib/booking_api/http_service.rb
+++ lib/booking_api/http_service.rb
@@ -17,6 +17,26 @@ module BookingApi
       end
     end
 
+    def secure_connection
+      @connection ||= begin
+        Faraday.new(:url => 'https://secure-distribution-xml.booking.com') do |faraday|
+          faraday.basic_auth username, password
+          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+          faraday.response :json, :content_type => /\bjson$/
+        end
+      end
+    end
+
+    def connection
+      @connection ||= begin
+        Faraday.new(:url => 'https://distribution-xml.booking.com') do |faraday|
+          faraday.basic_auth username, password
+          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+          faraday.response :json, :content_type => /\bjson$/
+        end
+      end
+    end
+
     def request_post(url, data)
       connection.post do |req|
         req.url url
@@ -27,6 +47,22 @@ module BookingApi
 
     def request_get(url, data)
       connection.get do |req|
+        req.url url
+        req.headers['Content-Type'] = 'application/json'
+        req.body = data.to_json
+      end
+    end
+
+    def secure_request_post(url, data)
+      secure_connection.post do |req|
+        req.url url
+        req.headers['Content-Type'] = 'application/json'
+        req.body = data.to_json
+      end
+    end
+
+    def secure_request_get(url, data)
+      secure_connection.get do |req|
         req.url url
         req.headers['Content-Type'] = 'application/json'
         req.body = data.to_json
--- lib/booking_api/payment.rb
+++ lib/booking_api/payment.rb
@@ -11,22 +11,22 @@ module BookingApi
 
     # Makes a booking for the specified hotel, room, guest.
     def process_booking(request_parameters: {})
-      http_service.request_post("/json/bookings.processBooking", request_parameters)
+      http_service.secure_request_post("/json/bookings.processBooking", request_parameters)
     end
 
     #Returns a list of the bookings asscociated to the guest.
     def guest_bookings(request_parameters: {})
-      http_service.request_post("/json/guestBookings", request_parameters)
+      http_service.secure_request_post("/json/guestBookings", request_parameters)
     end
 
     #Returns a list of the bookings that have been made through your account or with one of your affiliate IDs.
     def guest_booking_details(request_parameters: {})
-      http_service.request_post("/json/bookings.getBookingDetails", request_parameters)
+      http_service.secure_request_post("/json/bookings.getBookingDetails", request_parameters)
     end
 
         #Returns a list of the bookings that have been made through your account or with one of your affiliate IDs.
     def cancel_booking(request_parameters: {})
-      http_service.request_post("/json/bookings.cancelBooking", request_parameters)
+      http_service.secure_request_post("/json/bookings.cancelBooking", request_parameters)
     end
 
     private
