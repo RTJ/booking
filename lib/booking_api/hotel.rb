8098ccedffc83b3ba820792e685d1989de4f0663
Eugeniu Tambur <eugeniu@chatbots2go.com>
2017-07-18 16:03:00 +0200

    cleaned

--- lib/booking_api/hotel.rb
+++ lib/booking_api/hotel.rb
@@ -71,39 +71,3 @@ module BookingApi
 
   end
 end
-
-
-__END__
-
-params = {
-  checkin: Time.now.strftime("%F"),
-  checkout: (Time.now + (60 * 60 * 24 * 7 * 2)).strftime("%F"),
-  hotel_ids: [303762]
-}
-response = BookingApi::Client.new.get_hotel_availabillity(request_parameters: params)
-
-puts response.body
-{
-  "checkout": "2016-04-04",
-  "hotels": [
-    {
-      "room_min_price": {
-        "price": "750.00"
-      },
-      "hotel_id": "303762",
-      "hotel_currency_code": "EUR"
-    }
-  ],
-  "checkin": "2016-03-21",
-  "guest_groups": [
-    {
-      "guests": 2,
-      "children": [
-
-      ]
-    }
-  ],
-  "hotel_ids": [
-    "303762"
-  ]
-}
